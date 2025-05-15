4d9c8b2a7d8151faa392b61aefc954f8719024b3
poojitha-aredla <poojitha.a187@gmail.com>
2025-05-15 12:08:56 +0530

    Initial commit with all Eclipse project files

---
+++ src/com/tnsif/practice/Welcome.java
@@ -0,0 +1,12 @@
+//package com.tnsif.practice;
+//
+//public class Welcome {
+//
+//	public static void main(String[] args) {
+//		System.out.println("Welcome");
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}git remote -v
+git push -u origin main -v
